dbefde4f23a6a0ce7f0e0045b712c7a3748a48ea
Guocork <g1024536444@gmail.com>
2024-04-04 21:07:25 +0800

    增加按钮

--- src/page/Mine.vue
+++ src/page/Mine.vue
@@ -1,23 +1,25 @@
 <script setup>
- const items = [
-    {
-      name: 'African Elephant',
-      species: 'Loxodonta africana dasdsds ',
-      diet: 'Herbivore',
-      habitat: 'Savanna, Forests',
-      hhh:'5555 ccc'
-    },
-  ]
-
+const items = [
+  {
+    name: "African Elephant",
+    species: "Loxodonta africana dasdsds ",
+    diet: "Herbivore",
+    habitat: "Savanna, Forests",
+    hhh: "5555 ccc",
+  },
+];
 </script>
 
 <template>
-    <div class="container1">
-      <v-avatar :image="require('@/assets/avatar.png')" size="80"></v-avatar>
-    </div>
-    <div class="container2">
-      <p>Guo cork</p>
-    </div>
+  <div class="container1">
+    <v-avatar :image="require('@/assets/avatar.png')" size="80"></v-avatar>
+  </div>
+  <div class="container2">
+    <p>Guo cork</p>
+  </div>
+  <div class="container3">
+    <v-btn icon="$vuetify" variant="outlined"></v-btn>
+</div>
   <div><v-data-table-virtual :items="items"></v-data-table-virtual></div>
 </template>
 
@@ -31,11 +33,15 @@
 .container2 {
   text-align: center;
   margin-top: 20px;
-  margin-bottom: 50px;
+  margin-bottom: 10px;
 }
 
 p {
   font-size: 40px;
   font-weight: bold;
+}
+.container3 {
+    text-align: center;
+    margin-bottom: 10px;
 }
 </style>
