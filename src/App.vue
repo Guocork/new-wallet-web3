d267c2292228510c5ebc8eb1c3e0806cab4a0cc3
Guocork <g1024536444@gmail.com>
2024-04-04 15:22:41 +0800

    确定项目内容 引入路由

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <script setup>
-import Button from './components/Button.vue'
-import AccountList from './components/AccountList.vue'
+import HomePage from './page/HomePage.vue'
+import Transaction from "./page/Transaction.vue";
+import Mine from "./page/Mine.vue";
 import { ref, onMounted } from 'vue';
-import store from 'store2';
 
-const walletInfo = ref([]);
-
-onMounted(() => {
-  walletInfo.value = store.get("walletInfo") || [];
-});
-
+const active = ref(0);
+    const icon = [
+    {
+      active: 'gem',
+      inactive:
+        'gem-o',
+    },
+    {
+      active: 'gold-coin',
+      inactive:
+        'gold-coin-o',
+    },
+    {
+      active: 'contact',
+      inactive:
+        'contact-o',
+    }
+    ];
 
 </script>
 
 <template>
-<Button></Button>
-<AccountList :walletInfo="walletInfo"></AccountList>
+<van-tabbar v-model="active">
+  <van-tabbar-item>
+    <span>首页</span>
+    <template #icon="props">
+      <van-icon :name="props.active ? icon[0].active : icon[0].inactive" />
+    </template>
+  </van-tabbar-item>
+  <van-tabbar-item icon="gold-coin-o" @click="">
+    <span>交易</span>
+    <template #icon="props">
+      <van-icon :name="props.active ? icon[1].active : icon[1].inactive" />
+    </template>
+  </van-tabbar-item>
+  <van-tabbar-item icon="contact-o">
+    <span>我的</span>
+    <template #icon="props">
+      <van-icon :name="props.active ? icon[2].active : icon[2].inactive" />
+    </template>
+  </van-tabbar-item>
+</van-tabbar>
 </template>
 
 <style lang="less">
---
+++ src/page/Transaction.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Button from '../components/Button.vue'
+import AccountList from '../components/AccountList.vue'
+import { ref, onMounted } from 'vue';
+import store from 'store2';
+
+const walletInfo = ref([]);
+
+onMounted(() => {
+  walletInfo.value = store.get("walletInfo") || [];
+});
+
+</script>
+
+<template>
+<Button></Button>
+<AccountList :walletInfo="walletInfo"></AccountList>
+</template>
+
+<style scoped>
+
+</style>
