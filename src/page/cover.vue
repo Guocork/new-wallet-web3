d1c0687d537bce6858e5cedeec75b1a027dffae4
Guocork <g1024536444@gmail.com>
2024-04-13 23:55:53 +0800

    完善ui

---
+++ src/page/cover.vue
@@ -0,0 +1,16 @@
+<script setup>
+const imageUrl = require('@/assets/fengmian.png')
+</script>
+
+<template>
+<!-- <img :src="imageUrl" alt=""> -->
+<p>hello world!</p>
+<RouterLink to="/layout/HomePage">跳转</RouterLink>
+
+</template>
+
+<style scoped>
+/* .body {
+    background-image: "@/assets/fengmian.png";
+} */
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,28 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    { path: '/', redirect: '/HomePage' },
-  {
-    path: "/HomePage",
-    component: () => import('../page/HomePage.vue')
+  { path: '/',
+    component: () => import('../page/cover.vue')
   },
   {
-    path: "/Transaction",
-    component: () => import('../page/Transaction.vue')
+    path:"/layout",
+    redirect: '/layout/HomePage',
+    component:() => import('../page/layout.vue'),
+    children: [
+      {
+        path: "HomePage",
+        component: () => import('../page/HomePage.vue')
+      },
+      {
+        path: "Transaction",
+        component: () => import('../page/Transaction.vue')
+      },
+      {
+        path: "Mine",
+        component: () => import('../page/Mine.vue')
+      }
+    ]
   },
-  {
-    path: "/Mine",
-    component: () => import('../page/Mine.vue')
-  }
 ];
 
 const router = createRouter({
